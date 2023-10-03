ccd52f3dd739eb908e62fd274dbb96c9ffee3ff6
sridhardscv <sridhardscv@gmail.com>
2023-10-03 20:45:40 +0530

    Performance Info Notification

--- main.sh
+++ main.sh
@@ -30,6 +30,35 @@ root_attempts=$(grep -i "Failed password for root" /var/log/auth.log | wc -l)
 
 if [ "$root_attempts" -ge "$max_attempts" ]; then
     echo "Multiple root login attempts detected on $(hostname) at $(date)" 
-else
-    echo "Root Login Attempts Successfull"
+
 fi
+
+
+# Any File Events Occurs
+
+# directory_to_monitor="/home/sridhar"
+
+# inotifywait -m -e modify,access,attrib,close_write,delete,move,create "$directory_to_monitor" |
+#     while read -r directory_event; do
+#         echo "Directory breached in $directory_to_monitor at $(date): $directory_event" 
+#     done
+
+
+# Wheh the System Existing the Memory Usage 
+threshold=1000
+# memory usage in MB
+memory_usage=$(free -m | awk 'NR==2 {print $3}')
+
+if [ "$memory_usage" -gt "$threshold" ]; then
+    echo "High memory usage detected on $(hostname) at $(date). Memory used: ${memory_usage}MB"
+fi
+
+
+# Check memory usage
+memory_threshold=90
+memory_usage=$(free | awk '/Mem:/ {print $3/$2 * 100}' | cut -d. -f1)
+
+if [ "$memory_usage" -gt "$memory_threshold" ]; then
+    echo "High Memory Usage Alert" "Memory usage is above $memory_threshold% on $(hostname) at $(date)."
+fi
+
